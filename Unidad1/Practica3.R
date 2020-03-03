ed43edba199f25fe703e6889457c72900e1ce18a
Morga <morga9497@gmail.com>
2020-03-02 17:34:54 -0800

    Practica 3 actualizada

--- Unidad1/Practica3.R
+++ Unidad1/Practica3.R
@@ -15,6 +15,23 @@ profit.margin <- round(profitAt / revenue, 2) * 100
 profit.margin
 ##
 mean_pat <- mean(profitAt)
+mean_pat##
+revenue <- c(14574.49, 7606.46, 8611.41, 9175.41, 8058.65, 8105.44, 11496.28, 9766.09, 10305.32, 14379.96, 10713.97, 15433.50)
+expenses <- c(12051.82, 5695.07, 12319.20, 12089.72, 8658.57, 840.20, 3285.73, 5821.12, 6976.93, 16618.61, 10054.37, 3803.96)
+##
+profit <- revenue - expenses
+profit
+##
+tax <- round(profit * .30, 2) 
+tax
+##
+profitAt <- profit - tax
+profitAt
+##
+profit.margin <- round(profitAt / revenue, 2) * 100
+profit.margin
+##
+mean_pat <- mean(profitAt)
 mean_pat
 ##
 good.months <- mean_pat  <= profitAt
@@ -58,5 +75,49 @@ M <- rbind(
 M
 ##
 
+
+
+
+##
+good.months <- mean_pat  <= profitAt
+good.months
+##
+bad.months <- !good.months
+bad.months
+##
+best.month <- profitAt == max(profitAt)
+best.month
+##
+worst.month <- profitAt == min(profitAt)
+worst.month
+##
+revenue.1000 <- round(revenue / 1000, 0)
+expenses.1000 <- round(expenses / 1000, 0)
+profit.1000 <- round(profit / 1000, 0)
+profit.after.tax.1000 <-round(profitAt / 1000, 0)
+##
+revenue.1000
+expenses.1000
+profit.1000
+profit.after.tax.1000
+profit.margin
+good.months
+bad.months
+best.month
+worst.month
+##
+M <- rbind(
+  revenue.1000,
+  expenses.1000,
+  profit.1000,
+  profit.after.tax.1000,
+  profit.margin,
+  good.months,
+  bad.months,
+  best.month,
+  worst.month
+)
+M
+####
 
 
